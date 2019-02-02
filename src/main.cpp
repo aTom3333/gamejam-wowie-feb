67d6817ccc5e3709c9d4c301266e2033cb5b1d8a
PierreTSE <pierre--giraud@orange.fr>
2019-02-02 02:45:10 +0100

    main test

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,84 @@
 #include <SFML/Graphics.hpp>
 
 
-int main()
+int maintest()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-    
-    enum state {
-        Cool, Nul
+
+    enum state
+    {
+        Cool,
+        Nul
     };
-    
+
     GlobalClock::start();
-    
-    AnimatedEntity<state> entity(Cool, AnimatedSprite(1, sf::milliseconds(250), RessourceLoader::getTexture("sprites/walking_down.png"), sf::IntRect{340, 0, 340, 600}));
-    entity.setup(Nul, AnimatedSprite(2, sf::milliseconds(250), RessourceLoader::getTexture("sprites/ringing_down.png"), sf::IntRect{0, 0, 340, 600}));
-    
-    while (window.isOpen())
+
+    //AnimatedEntity<state> entity(Cool, AnimatedSprite(1, sf::milliseconds(250), RessourceLoader::getTexture("sprites/walking_down.png"), sf::IntRect{340, 0, 340, 600}));
+    //entity.setup(Nul, AnimatedSprite(2, sf::milliseconds(250), RessourceLoader::getTexture("sprites/ringing_down.png"), sf::IntRect{0, 0, 340, 600}));
+
+    std::vector<sf::RectangleShape> shapes;
+
+    const int n = 10;
+
+    for(size_t i = 0 ; i < n ; ++i) {
+        for(size_t j = 0 ; j < n ; ++j)
+        {
+			shapes.emplace_back(sf::Vector2f(n/2, n/2));
+            shapes.back().setFillColor(sf::Color::Green);
+            shapes.back().setPosition(n*i, n*j);
+        } 
+    }
+
+    sf::View mainView(sf::FloatRect(0, 0, 1200, 600));
+
+    while(window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while(window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if(event.type == sf::Event::Closed)
                 window.close();
-            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A)
-                entity.setState(static_cast<state>(1 - entity.getState()));
+            if(event.type == sf::Event::KeyPressed)
+            {
+                sf::Time elapsedTime = GlobalClock::lapTime();
+                const int speed = 500;
+
+                switch(event.key.code)
+                {
+                    case sf::Keyboard::Up :
+                        mainView.move(0, speed * elapsedTime.asSeconds());
+                        break;
+                    case sf::Keyboard::Right :
+                        mainView.move(speed * elapsedTime.asSeconds(), 0);
+                        break;
+                    case sf::Keyboard::Down :
+                        mainView.move(0, - speed * elapsedTime.asSeconds());
+                        break;
+                    case sf::Keyboard::Left :
+                        mainView.move(- speed * elapsedTime.asSeconds(), 0);
+                        break;
+                    default :
+                        break;
+                }
+            }
         }
 
         GlobalClock::lap();
 
         window.clear();
-        entity.draw(window);
+
+        window.setView(mainView);
+
+        for(auto& shape : shapes)
+            window.draw(shape);
+
         window.display();
     }
 
+    return 0;
+}
+
+int main()
+{
     return 0;
 }
