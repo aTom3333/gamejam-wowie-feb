69e8ba09b7236121db691797737e1b3ee23ee75a
PierreTSE <pierre--giraud@orange.fr>
2019-02-02 01:37:25 +0100

    Utilities

--- src/Utilities.hpp
+++ src/Utilities.hpp
@@ -4,21 +4,16 @@
 #include <SFML/Graphics.hpp>
 #include <filesystem>
 
+template<typename T>
+void centerOrigin(T& t) { t.setOrigin(t.getLocalBounds().width / 2.0, t.getLocalBounds().height / 2.0); }
+
 sf::Vector2f normalize(const sf::Vector2f& source);
-void fit(sf::Sprite& s);
+void         fit(sf::Sprite&               s);
 
-sf::Color static makeHSV(int hue, float sat, float val, float alpha);
-
-// hue: 0-360°; sat: 0.f-1.f; val: 0.f-1.f
-sf::Color makeHSV(int hue, float sat, float val, float alpha);
+sf::Color makeHSV(int hue, float sat, float val, float alpha); // hue: 0-360°; sat: 0.f-1.f; val: 0.f-1.f
 
 int random(int min, int max);
 int random(int max);
-
-template<typename T>
-void centerOrigin(T& t) {
-    t.setOrigin(t.getLocalBounds().width/2.0, t.getLocalBounds().height/2.0);
-}
 
 std::filesystem::path strip_root(const std::filesystem::path& p);
 
